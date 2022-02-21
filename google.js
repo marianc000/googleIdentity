3756a4bf3b4cb995df98a10304be24e5b9669d30
Marian <marian.caikovski@gmail.com>
2022-02-21 21:04:26 +0100

    first

--- google.js
+++ google.js
@@ -1,14 +1,9 @@
 import { OAuth2Client } from 'google-auth-library';
 
 const client = new OAuth2Client(process.env.CLIENT_ID);
-export async function verify(idToken) {
-  const ticket = await client.verifyIdToken({
+export function verify(idToken) {
+  return client.verifyIdToken({
     idToken,
     audience: process.env.CLIENT_ID
-  });
-  const payload = ticket.getPayload();
-  const userid = payload['sub'];
-  // If request specified a G Suite domain:
-  // const domain = payload['hd'];
-  return payload;
+  }).then(ticket => ticket.getPayload());
 }
